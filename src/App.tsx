506fd2bb8a566123459ffd3f9d0d31eb14223b30
Jiadi Tao <jiadi.tao@uwaterloo.ca>
2025-02-28 13:59:06 -0500

    404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css'
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<div>test1</div>} />
       <Route path="/about" element={<div>test2</div>} />
-      <Route path="*" element={<div>test</div>} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
