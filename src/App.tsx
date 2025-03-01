0f488f507b4a99b2f0b98ab5f2f96375edec9fbd
Jiadi Tao <jiadi.tao@uwaterloo.ca>
2025-03-01 13:33:39 -0500

    save

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,15 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<div>TODO USER LOGIN PAGE</div>} />
-        <Route path="/organization" element={<div>TODO ORG LOGIN PAGE</div>} />
-        <Route path="/posting" element={<div>sdf</div>} />
+        {/* 
+             for all users pages, prepend with /user
+             for all company pages, prepend with /org
+        */}
+        <Route path="/user" element={<div>TODO USER LOGIN PAGE</div>} />
+        <Route path="/org" element={<div>TODO ORG LOGIN PAGE</div>} />
+
+        <Route path="/user/dashboard" element={<div>sdf</div>} />
+        <Route path="/org/dashboard" element={<div>sdf</div>} />
       </Routes>
     </Router>
   );
