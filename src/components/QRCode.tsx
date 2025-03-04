7cc27ffd697ce62783ccdc6c07c24dcf5dbff8b8
Jiadi Tao <jiadi.tao@uwaterloo.ca>
2025-03-03 22:21:23 -0500

    split 2fa into 2 views

--- src/components/QRCode.tsx
+++ src/components/QRCode.tsx
@@ -1,19 +1,51 @@
 import { QRCodeSVG } from "qrcode.react";
-import { Card, CardContent } from "@mui/material";
+import { Button, Card, CardContent, TextField } from "@mui/material";
+import { useEffect, useState } from "react";
+import * as OTPAuth from "otpauth";
 
 type Props = {
-  qrCodeStr: string,
+  goBack: () => void,
+  submit2FA: (code: string) => void
 }
 
-const QRCodeGenerator = ({ qrCodeStr }: Props) => {
+const QRCodeGenerator = ({ goBack, submit2FA }: Props) => {
+  const [form, setForm] = useState({ oneTimeCode: "" });
+  const [twoFAOPT, setTwoFaOpt] = useState<{ secret: string, qrCode: string } | null>(null);
+
+  useEffect(() => {
+    const secret = new OTPAuth.Secret().base32;
+    let totp = new OTPAuth.TOTP({
+      secret,
+      label: "2FA",
+      issuer: "ByteBreakers",
+    });
+    setTwoFaOpt({ secret, qrCode: totp.toString() });
+  }, []);
+
   return (
-    <Card elevation={0} sx={{ maxWidth: 400, mx: "auto", mt: 4, p: 2, textAlign: "center" }}>
-      <h4>For your security, 2FA with Google Authenticator is required when signing up</h4>
-      <h4>Scan the QR code below and enter the code into the field on the left</h4>
+    <Card elevation={0} sx={{ maxWidth: 400, mx: "auto", p: 2, textAlign: "center" }}>
+      <h4>For your security, 2FA with Google Authenticator is required when signing up. Scan the QR code and enter the security code below</h4>
       <CardContent>
-        {qrCodeStr && <QRCodeSVG value={qrCodeStr} size={200} />}
+        {twoFAOPT?.qrCode && <QRCodeSVG value={twoFAOPT.qrCode} size={200} />}
+        <form>
+          <TextField
+            label="2FA Code"
+            placeholder="Enter the 6 digit Google Authenticator Code Here"
+            type="text"
+            name="twoFaCode"
+            fullWidth
+            margin="normal"
+            value={form.oneTimeCode}
+            onChange={(e) => setForm({ ...form, oneTimeCode: e.target.value })}
+            required
+          />
+          <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: 10 }}>
+            <Button onClick={goBack} variant="outlined" color="primary">Go Back</Button>
+            <Button onClick={() => submit2FA(form.oneTimeCode)} variant="contained" color="primary">Submit</Button>
+          </div>
+        </form>
       </CardContent>
-    </Card>
+    </Card >
   );
 };
 
